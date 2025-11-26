9a9a47b87f4c4b8bb14204bf51fcf47cfdcb8093
jsw4335 <96003896+jsw4335@users.noreply.github.com>
2025-11-27 04:04:12 +0900

    로그인,회원가입 수정(오류 메세지 수정 및 회원가입시 생년월일도 받아오는걸로 수정), DB포트번호 수정에 따른 포트번호 추가

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import usersRouter from "./routes/users.route";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:3000",//이 부분 프론트 서버랑 주소 맞추는 건데 예성님께서 포트번호 수정하셔서 사용하시고 테스트 해주세요
+    credentials: true
+}));
 app.use(express.json());
 
-// 라우터 등록
 app.use("/users", usersRouter);
 
 export default app;
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const signup = async (userData: any) => {
-    const { name, email, password } = userData;
+    const { name, email, password,birth_date } = userData;
 
     const [existUser]: any = await pool.query(
         "SELECT id FROM users WHERE email = ?",
@@ -16,8 +16,8 @@ export const signup = async (userData: any) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const [result]: any = await pool.query(
-        "INSERT INTO users (email, password, name) VALUES (?, ?, ?)",
-        [email, hashedPassword, name]
+        "INSERT INTO users (email, password, name, birth_date) VALUES (?, ?, ?, ?)",
+        [email, hashedPassword, name, birth_date]
     );
 
     return { id: result.insertId, email, name };
@@ -31,12 +31,12 @@ export const login = async (userData: any) => {
         [email]
     );
 
-    if (rows.length === 0) throw new Error("존재하지 않는 이메일입니다.");
+    if (rows.length === 0) throw new Error("아이디 또는 비밀번호가 잘못되었습니다.");
 
     const user = rows[0];
 
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) throw new Error("비밀번호가 틀렸습니다.");
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) throw new Error("아이디 또는 비밀번호가 잘못되었습니다.");
 
     const token = jwt.sign(
         { id: user.id, email: user.email },
